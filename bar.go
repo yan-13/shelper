55c0dc1375c6be01d65d6c9419f9a5809f81ca8c
zhaojiayong <zhaojiayong@bilibili.com>
2021-11-02 16:28:36 +0800

    hotfix Bar

--- bar.go
+++ bar.go
@@ -2,6 +2,8 @@ package shelper
 
 import (
     "fmt"
+    "strconv"
+    "strings"
 )
 
 type Bar struct {
@@ -12,37 +14,41 @@ type Bar struct {
     graph   string //显示符号
 }
 
-func (bar *Bar) NewOption(start, total int) {
-    bar.cur = start
+const (
+    defaultGraph = "█"
+    maxGraphNum  = 50
+)
+
+func (bar *Bar) New(total int) {
+    bar.cur = 0
     bar.total = total
-    if bar.graph == "" {
-        bar.graph = "█"
-    }
+    bar.graph = defaultGraph
     bar.percent = bar.getPercent()
-    for i := 0; i < int(bar.percent); i += 2 {
-        bar.rate += bar.graph //初始化进度条位置
+    bar.rate = bar.getRate()
+}
+
+func (bar *Bar) SetGraph(graph string) {
+    if 1 != len(graph) {
+        graph = defaultGraph
     }
+    bar.graph = graph
+}
+
+func (bar *Bar) Play(cur int) {
+    bar.cur = cur
+    bar.percent = bar.getPercent()
+    bar.rate = bar.getRate()
+    fmt.Printf("\r[%-"+strconv.Itoa(maxGraphNum)+"s]%3d%%  %8d/%d", bar.rate, bar.percent, bar.cur, bar.total)
+}
+
+func (bar *Bar) Finish() {
+    fmt.Println()
 }
 
 func (bar *Bar) getPercent() int64 {
     return int64(float32(bar.cur) / float32(bar.total) * 100)
 }
 
-func (bar *Bar) NewOptionWithGraph(start, total int, graph string) {
-    bar.graph = graph
-    bar.NewOption(start, total)
-}
-
-func (bar *Bar) Play(cur int) {
-    bar.cur = cur
-    last := bar.percent
-    bar.percent = bar.getPercent()
-    if bar.percent != last && bar.percent%2 == 0 {
-        bar.rate += bar.graph
-    }
-    fmt.Printf("\r[%-50s]%3d%%  %8d/%d", bar.rate, bar.percent, bar.cur, bar.total)
-}
-
-func (bar *Bar) Finish() {
-    fmt.Println()
+func (bar *Bar) getRate() string {
+    return strings.Repeat(bar.graph, int(bar.percent*maxGraphNum/100))
 }
